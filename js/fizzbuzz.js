f1feecc3c30b3dfb46932ac734cac2eec0d25237
charleneclee <cclee@google.com>
2017-05-08 15:41:05 -0700

    finished js conditionals lab

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function fizzbuzz(num){
-  
+    if (num % 5 === 0 && num % 3 === 0){
+    return "FizzBuzz"
+  } else if (num % 3 === 0) {
+    return "Fizz"
+  } else if (num % 5 === 0) {
+    return "Buzz"
+  } else return num
 }
